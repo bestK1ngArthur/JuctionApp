c3ef5191329d1fbbc2ef0472b0532a0ab917d51f
Artem Belkov <bestk1ngarthur@aol.com>
2020-11-06 19:11:47 +0300

    Initial Commit

---
+++ JunctionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JunctionApp
+//
+//  Created by Artem Belkov on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
